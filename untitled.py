1798dab73a4fbed5b0cbf9de5002eb5be5551001
GxM20 <gabe.morano@gmail.com>
2023-12-19 12:31:16 -0800

    pushing main notebook

---
+++ untitled.py
@@ -0,0 +1,14 @@
+# import functions
+
+import numpy
+import numpy_financial as npf
+from pathlib import Path
+
+# initialize variables
+
+total_months = 0
+total_profit = 0
+average_change = 0
+greatest_increase = 0
+greatest_decrease = 0
+
